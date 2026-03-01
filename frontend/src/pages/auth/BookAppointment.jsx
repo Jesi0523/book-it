efa71230d333d97a02d1213c6cf158cfadaacdea
itzel04 <itzel.perezmrl@uanl.edu.mx>
2026-03-01 11:52:32 -0600

    style: responsive main page

--- frontend/src/pages/auth/BookAppointment.jsx
+++ frontend/src/pages/auth/BookAppointment.jsx
@@ -22,12 +22,15 @@ const employsDummy = [{name: 'Oliver Hansen', pfp: photo},{name:'Van Henry', pfp
 const hoursDummy =[ '09:00-10:00am', '10:30-11:30am', '12:00-13:00pm', '13:00-14:00pm', '15:00-16:00pm'];
 // ****************************
 
+
 function BookAppointment()
 {
+    const linearDegraded= 'linear-gradient(180deg, #2c2e5b 0%, #1c1e51 100%)';
+    const colorBorder='2px solid #2c2e5bba';
     return(
         <>
             <NavBar/>
-            <Box sx={{p:7}}>
+            <Box sx={{p:{xs: 2, md:7}}}>
                 <Box sx={{px: 3}}>
                     <Title color='text.primary' align="{xs: 'center', md: 'start'}">Agenda tu cita</Title>
                 </Box>
@@ -38,15 +41,15 @@ function BookAppointment()
                         <Calendar/>
                     </Grid>
 
-                    <Grid size={{xs: 12, md: 8}} sx={{p:5, display: 'flex', flexDirection: 'column', gap: {xs:1, md:5}}}>
-                        <Grid container sx={{display: 'flex'}}>
-                            <Grid size={{xs: 12, md: 8}}>
+                    <Grid size={{xs: 12, md: 8}} sx={{p:{xs:0.5 ,md:5}, display: 'flex', flexDirection: 'column', gap: {xs:1, md:5}}}>
+                        <Grid container spacing={2} sx={{display: 'flex'}}>
+                            <Grid size={8}>
                                 <Combobox name='Selecciona un servicio' array={servicesDummy}/>
                             </Grid>
-                            <Grid size={{xs: 12, md: 4}}>
+                            <Grid size={4}>
                                 <Box sx={{display: 'flex', alignItems: 'center', flexDirection: 'column', color: 'primary.main', justifyContent: 'center'}}>
                                     <Text children= 'Costo' color= 'primary.main' />
-                                    <Box sx={{background: 'linear-gradient(180deg, #2c2e5b 0%, #13154d 100%)', py:1, px:2, borderRadius: '25%' }}>
+                                    <Box sx={{background: linearDegraded, py:1, px:2, borderRadius: '25%' }}>
                                         $$$$
                                     </Box>
                                 </Box>
@@ -58,25 +61,25 @@ function BookAppointment()
                 </Grid>
 
                 {/* Datos del cliente */}
-                <Box sx={{px:5, py:1}}>
+                <Box sx={{px:{xs: 3,md:5}, py:1}}>
                     <Title children= 'Datos del cliente' size='16' textTransform='capitalize'></Title>
-                    <Box sx={{p: 3, my:3, background: 'linear-gradient(180deg, #2c2e5b 0%, #13154d 100%)', borderRadius: '15px',display: 'flex', flexDirection: 'column', gap:5 }}>
-                        <Grid container spacing={5}>
+                    <Box sx={{p:{xs:1.5 ,md:3}, my:3, background: linearDegraded, borderRadius: '15px',display: 'flex', flexDirection: 'column', gap:5 }}>
+                        <Grid container spacing={{xs:1 ,md:5}}>
                             <Grid size={{xs: 12, md: 6}}>
-                                <TextInput label='Nombre Completo' placeholder='Ingresa tu nombre' background='linear-gradient(180deg, #2c2e5b 0%, #13154d 100%)' border='#2c2e5b7d'/>
+                                <TextInput label='Nombre Completo' placeholder='Ingresa tu nombre' background={linearDegraded} border={colorBorder}/>
                             </Grid>
-                            <Grid size={{xs: 6, md: 2}}>
-                                <TextInput label='Edad' placeholder='Ej:18 años' type='number' background='linear-gradient(180deg, #2c2e5b 0%, #13154d 100%)' border='#2c2e5b7d'/>
+                            <Grid size={{xs: 4, md: 2}}>
+                                <TextInput label='Edad' placeholder='Ej:18 años' type='number' background={linearDegraded} border={colorBorder}/>
                             </Grid>
                             {/* TODO: Cambiar por otro display */}
-                            <Grid size={{xs: 6, md: 4}}>
-                                <TextInput label='Número telefónico' type='number' placeholder='Ejemplo' background='linear-gradient(180deg, #2c2e5b 0%, #13154d 100%)' border='#2c2e5b7d'/>
+                            <Grid size={{xs: 8, md: 4}}>
+                                <TextInput label='Número telefónico' type='number' placeholder='Ejemplo' background={linearDegraded} border={colorBorder}/>
                             </Grid>
                         </Grid>
-                        <Grid container spacing={5} sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                            <Grid size={2}><GenderSelect background='linear-gradient(180deg, #2c2e5b 0%, #13154d 100%)' border='#2c2e5b7d'/></Grid>
-                            <Grid size={6}>
-                                <TextInput label='Correo electrónico' type='email' placeholder='Ejemplo' background='linear-gradient(180deg, #2c2e5b 0%, #13154d 100%)' border='#2c2e5b7d'/>
+                        <Grid container spacing={{xs: 1,md:5}} sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                            <Grid size={{xs:4, md:2}}><GenderSelect background={linearDegraded} border={colorBorder}/></Grid>
+                            <Grid size={{xs:8, md:6}}>
+                                <TextInput label='Correo electrónico' type='email' placeholder='Ejemplo' background={linearDegraded} border={colorBorder}/>
                             </Grid>
                         </Grid>
                     </Box>
@@ -85,7 +88,7 @@ function BookAppointment()
                 {/* Confirmación y muestra de datos */}
                 <Box sx={{px:5}}>
                     <Title children= 'Se agendará una cita con la siguiente fecha:' size='16' textTransform='capitalize' align='center'/>
-                    <Box sx={{p: 3, my:3, background: 'linear-gradient(180deg, #2c2e5b 0%, #13154d 100%)', borderRadius: '15px',display: 'flex', flexDirection: 'column', gap:1 }}>
+                    <Box sx={{p: {xs:1, md:3}, my:3, background: linearDegraded, borderRadius: '15px',display: 'flex', flexDirection: 'column', gap:1 }}>
                         <Text children= 'Febrero 11, 2026 9:00am a 10:00am.' size='18' align='center'/>
                         <Text children= 'Empleado 1' size='16' align='center'/>
                     </Box>

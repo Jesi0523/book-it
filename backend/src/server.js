741663278392a3549fbea71f1e35aa0b0597183b
SofiaAyalanis <sofi-aaa@hotmail.com>
2026-03-01 15:14:41 -0600

    refactor: estructuración de carpetas del backend y server.js dentro de src

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 // Rutas
 app.use("/api/usuarios", usuarioRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Conectar la base de datos
 connectDB()
